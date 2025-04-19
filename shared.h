33968347e1c99dc6f9dc5c1a9d81e07703d50ae3
Lbeenc <Lbeenc@gmail.com>
2025-04-18 21:40:47 -0500

    Create shared.h

---
+++ shared.h
@@ -0,0 +1,34 @@
+// shared.h
+#ifndef SHARED_H
+#define SHARED_H
+
+#include <sys/types.h>
+
+#define MAX_PROCESSES 18
+#define MSG_KEY 0x1234
+#define SHM_KEY 0x5678
+#define QUANTUM_BASE 10000000
+#define MAX_TOTAL_PROCS 40
+#define NUM_QUEUES 3
+
+typedef struct {
+    unsigned int seconds;
+    unsigned int nanoseconds;
+} SimClock;
+
+typedef struct {
+    int occupied;
+    pid_t pid;
+    int startSeconds;
+    int startNano;
+    int totalCpuTime;
+    int lastBurst;
+    int queueLevel;
+    int blocked;
+} PCB;
+
+typedef struct {
+    long mtype;
+    int usedTime;
+    int willTerminate;
+} Message;
