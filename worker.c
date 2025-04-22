21aa4e641bf9feace9136eb6f81d054c34554633
Lbeenc <Lbeenc@gmail.com>
2025-04-21 19:06:30 -0500

    Update worker.c

--- worker.c
+++ worker.c
@@ -3,22 +3,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/msg.h>
-#include <time.h>
 #include "shared.h"
 
 int main() {
     srand(getpid());
     int msgID = msgget(MSG_KEY, 0666);
-
     Message msg;
-    msgrcv(msgID, &msg, sizeof(Message) - sizeof(long), getpid(), 0);
 
-    int used = rand() % msg.usedTime;
+    if (msgrcv(msgID, &msg, sizeof(Message) - sizeof(long), getpid(), 0) == -1) {
+        perror("worker msgrcv");
+        exit(1);
+    }
+
+    int used = rand() % msg.usedTime + 1;
     msg.mtype = getppid();
     msg.usedTime = used;
-    msg.willTerminate = rand() % 5 == 0 ? 1 : 0;
+    msg.willTerminate = (rand() % 5 == 0);
 
-    msgsnd(msgID, &msg, sizeof(Message) - sizeof(long), 0);
+    if (msgsnd(msgID, &msg, sizeof(Message) - sizeof(long), 0) == -1) {
+        perror("worker msgsnd");
+        exit(1);
+    }
     return 0;
 }
-
